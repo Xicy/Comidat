ac38e613c811f9b9ec38b163896ce4e0b288ffeb
Umut Akkaya <umutakkaya1996@gmail.com>
2018-01-19 19:51:58 +0300

    added exception handler

---
+++ Comidat.Util/Diagnostics/ExceptionHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Comidat.Diagnostics
+{
+    public static class ExceptionHandler
+    {
+        public static void InstallExceptionHandler()
+        {
+            TaskScheduler.UnobservedTaskException += (s, e) =>
+            {
+                e.SetObserved();
+                Logger.Exception(e.Exception);
+            };
+
+            if (Debugger.IsAttached) return;
+            AppDomain.CurrentDomain.FirstChanceException += (s, e) =>
+                Logger.Exception(e.Exception, e.Exception.Message);
+
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+            {
+                var ee = (Exception)e.ExceptionObject;
+                Logger.Exception(ee, ee.Message);
+            };
+
+        }
+    }
+}
