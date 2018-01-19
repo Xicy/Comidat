ed8a0a846f87a845f5c183f61bff0b42c783dd9f
Umut Akkaya <umutakkaya1996@gmail.com>
2018-01-19 19:51:44 +0300

    Added simple reporter

---
+++ Comidat.Windows.Client/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Comidat.Windows.Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.exportButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(12, 12);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker.TabIndex = 0;
+            // 
+            // exportButton
+            // 
+            this.exportButton.Location = new System.Drawing.Point(218, 10);
+            this.exportButton.Name = "exportButton";
+            this.exportButton.Size = new System.Drawing.Size(75, 23);
+            this.exportButton.TabIndex = 1;
+            this.exportButton.Text = "Dışarı Aktar";
+            this.exportButton.UseVisualStyleBackColor = true;
+            this.exportButton.Click += new System.EventHandler(this.exportButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 41);
+            this.Controls.Add(this.exportButton);
+            this.Controls.Add(this.dateTimePicker);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Comidat Reporter";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Button exportButton;
+    }
+}
+
---
+++ Comidat.Windows.Client/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Comidat.Data;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace Comidat.Windows.Client
+{
+    public partial class Form1 : Form
+    {
+        private static readonly DatabaseContext Database = new DatabaseContext();
+        private readonly BaseFont baseFont;
+        public Form1()
+        {
+            InitializeComponent();
+            baseFont = BaseFont.CreateFont(Environment.GetEnvironmentVariable("windir") + @"\fonts\Arial.TTF", BaseFont.IDENTITY_H, true);
+            dateTimePicker.MinDate = Database.TBLPositions.Min(p => p.RecordDateTime);
+            dateTimePicker.MaxDate = Database.TBLPositions.Min(p => p.RecordDateTime);
+            dateTimePicker.Value = DateTime.Now.Date == dateTimePicker.MaxDate ? DateTime.Today : dateTimePicker.MaxDate;
+        }
+
+        private void exportButton_Click(object sender, EventArgs e)
+        {
+
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            using (MemoryStream ms = new MemoryStream())
+            using (Document document = new Document(PageSize.A4, 25, 25, 30, 30))
+            using (PdfWriter writer = PdfWriter.GetInstance(document, ms))
+            {
+                sfd.Filter = @"PDF|*.pdf";
+                if (sfd.ShowDialog() != DialogResult.OK) return;
+
+                var test = Database.TBLPositions
+                    .Where(p => p.RecordDateTime.Date == dateTimePicker.Value.Date)
+                    .OrderBy(p => p.TagId)
+                    .ThenByDescending(p => p.RecordDateTime)
+                    .Join(Database.TBLMaps, p => p.MapId, m => m.Id,
+                        (p, m) => new { Position = p, Map = m }) //Join Map
+                    .Join(Database.TBLTags, p => p.Position.TagId, t => t.Id,
+                        (p, t) => new { p.Position, p.Map, Tag = t }); //Join Tags
+
+
+                document.Open();
+                writer.DirectContent.SetFontAndSize(baseFont, 12);
+                PdfPTable table = new PdfPTable(6);
+                table.AddCell("isim");
+                table.AddCell("Soyisim");
+                table.AddCell("Harita");
+                table.AddCell("X");
+                table.AddCell("Y");
+                table.AddCell("Saat");
+                foreach (var obj in test)
+                {
+                    table.AddCell(obj.Tag.TagFirstName);
+                    table.AddCell(obj.Tag.TagLastName);
+                    table.AddCell(obj.Map.MapName);
+                    table.AddCell(obj.Position.d_XPosition.ToString());
+                    table.AddCell(obj.Position.d_yPosition.ToString());
+                    table.AddCell(obj.Position.RecordDateTime.TimeOfDay.ToString(@"hh\:mm\:ss"));
+                }
+
+                document.Add(table);
+                document.Close();
+                writer.Close();
+                ms.Close();
+
+                File.WriteAllBytes(sfd.FileName, ms.GetBuffer());
+            }
+            MessageBox.Show("Dışarı Aktarma Başarılı Birşekilde Tamamlandı.", "Dışarı Aktarma", MessageBoxButtons.OK);
+        }
+    }
+}
