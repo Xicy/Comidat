da421abda95c17287198b2f5c14590cb6a41d8d2
Umut Akkaya <umutakkaya1996@gmail.com>
2018-01-20 12:49:20 +0300

    added obfuscation properties

--- Comidat.Windows.Client/Properties/AssemblyInfo.cs
+++ Comidat.Windows.Client/Properties/AssemblyInfo.cs
@@ -33,3 +33,11 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: Obfuscation(Exclude = false, Feature = "strong name key:../../Comidat.snk")]
+[assembly: Obfuscation(Exclude = false, Feature = "+constants")]
+[assembly: Obfuscation(Exclude = false, Feature = "+anti ildasm")]
+[assembly: Obfuscation(Exclude = false, Feature = "+anti debug")]
+[assembly: Obfuscation(Exclude = false, Feature = "+resources(mode=dynamic)")]
+[assembly: Obfuscation(Exclude = false, Feature = "+ctrl flow(predicate=expression)")]
+[assembly: Obfuscation(Exclude = false, Feature = "+rename(mode=reversible,password=Umut1996)")]
