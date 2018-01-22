cc681ab2ba3a2e91505afeaaff28f56d77b42b84
Umut Akkaya <umutakkaya1996@gmail.com>
2018-01-22 18:40:32 +0300

    Name changed client to reporter

--- Comidat.Windows.Reporter/Properties/AssemblyInfo.cs
+++ Comidat.Windows.Reporter/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Comidat Client")]
+[assembly: AssemblyTitle("Comidat Reporter")]
 [assembly: AssemblyDescription("Personal Trace Report Program")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Xicy")]
-[assembly: AssemblyProduct("Comidat.Windows.Client")]
+[assembly: AssemblyProduct("Comidat.Windows.Reporter")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
