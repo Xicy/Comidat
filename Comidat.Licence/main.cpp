f3b65b7d9104a78c1251e785c8ddc1cae5e6d9d8
Umut Akkaya <umutakkaya1996@gmail.com>
2018-01-26 04:52:58 +0300

    updated license header export delegate

--- Comidat.Licence/main.cpp
+++ Comidat.Licence/main.cpp
@@ -1,1 +1,40 @@
 #include "main.h"
+#include <stdio.h>
+
+EXPORT(void) DoWork(ProgressCallback progressCallback)
+{
+	int counter = 0;
+	if (progressCallback)
+	{
+		for (; counter <= 100; counter++)
+		{
+			progressCallback(counter);
+
+		}
+	}
+}
+
+EXPORT(void) ProcessFile(GetFilePathCallback getPath)
+{
+	if (getPath)
+	{
+		// get file path...
+		char* path = getPath((char*)"Text Files|*.txt");
+		// open the file for reading
+		FILE *file = fopen(path, "r");
+		// read buffer
+		char line[1024];
+
+		// print file info to the screen
+		printf("File path: %s\n", path ? path : "N/A");
+		printf("File content:\n");
+
+		while (fgets(line, 1024, file) != NULL)
+		{
+			printf("%s", line);
+		}
+
+		// close the file
+		fclose(file);
+	}
+}
--- Comidat.Licence/main.h
+++ Comidat.Licence/main.h
@@ -1,9 +1,17 @@
-#ifndef MAIN_HPP
-#define MAIN_HPP
-extern "C"
-{
-	__declspec(dllexport) int __stdcall add(int a, int b) {
-		return a + b;
-	}
+#ifdef _WIN32
+#define _CRT_SECURE_NO_DEPRECATE
+#endif
+#define EXPORT(TYPE) __declspec(dllexport) TYPE __stdcall
+#define DELEGATE(RET,NAME,IN) typedef RET(__stdcall *NAME)IN;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+	DELEGATE(void, ProgressCallback, (int));
+	DELEGATE(char*, GetFilePathCallback, (char* filter));
+
+	EXPORT(void) DoWork(ProgressCallback progressCallback);
+	EXPORT(void) ProcessFile(GetFilePathCallback getPath);
+#ifdef __cplusplus
 }
 #endif
