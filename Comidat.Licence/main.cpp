e30af736b2ca2ae3b71c9ae8aa6f2ea1cef496c0
Umut Akkaya <umutakkaya1996@gmail.com>
2018-01-29 14:41:27 +0300

    added cpu info asm function

--- Comidat.Licence/main.cpp
+++ Comidat.Licence/main.cpp
@@ -1,40 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 
-EXPORT(void) DoWork(ProgressCallback progressCallback)
-{
-	int counter = 0;
-	if (progressCallback)
-	{
-		for (; counter <= 100; counter++)
-		{
-			progressCallback(counter);
-
-		}
-	}
-}
-
-EXPORT(void) ProcessFile(GetFilePathCallback getPath)
-{
-	if (getPath)
-	{
-		// get file path...
-		char* path = getPath((char*)"Text Files|*.txt");
-		// open the file for reading
-		FILE *file = fopen(path, "r");
-		// read buffer
-		char line[1024];
-
-		// print file info to the screen
-		printf("File path: %s\n", path ? path : "N/A");
-		printf("File content:\n");
-
-		while (fgets(line, 1024, file) != NULL)
-		{
-			printf("%s", line);
-		}
-
-		// close the file
-		fclose(file);
-	}
+EXPORT(int*) GetCPUInfo(unsigned i) {
+	int regs[4] = { 0 };
+#ifdef _WIN32
+	__cpuid(regs, i);
+#else
+	asm volatile ("cpuid" : "=a" (regs[0]), "=b" (regs[1]), "=c" (regs[2]), "=d" (regs[3]) : "a" (i), "c" (0));
+#endif
+	return regs;
 }
--- Comidat.Licence/main.h
+++ Comidat.Licence/main.h
@@ -1,17 +1,21 @@
-#ifdef _WIN32
-#define _CRT_SECURE_NO_DEPRECATE
-#endif
+#ifndef CPUID_H
+#define CPUID_H
 #define EXPORT(TYPE) __declspec(dllexport) TYPE __stdcall
 #define DELEGATE(RET,NAME,IN) typedef RET(__stdcall *NAME)IN;
-
+#ifdef _WIN32
+#include <limits.h>
+#include <intrin.h>
+typedef unsigned __int32  uint32_t;
+#else
+#include <stdint.h>
+#endif
 #ifdef __cplusplus
 extern "C" {
 #endif
-	DELEGATE(void, ProgressCallback, (int));
-	DELEGATE(char*, GetFilePathCallback, (char* filter));
-
-	EXPORT(void) DoWork(ProgressCallback progressCallback);
-	EXPORT(void) ProcessFile(GetFilePathCallback getPath);
+	//Header start here
+	EXPORT(int*) GetCPUInfo(unsigned);
+	//Header end here
 #ifdef __cplusplus
 }
 #endif
+#endif // CPUID_H
