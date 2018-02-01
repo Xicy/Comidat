8dee1600bef7b759766570b746b9927807c3939e
Umut Akkaya <umutakkaya1996@gmail.com>
2018-02-02 01:16:52 +0300

    remove unneccesery detail

--- Comidat.Util/Diagnostics/ExceptionHandler.cs
+++ Comidat.Util/Diagnostics/ExceptionHandler.cs
@@ -16,12 +16,12 @@ namespace Comidat.Diagnostics
 
             if (Debugger.IsAttached) return;
             AppDomain.CurrentDomain.FirstChanceException += (s, e) =>
-                Logger.Exception(e.Exception, e.Exception.Message);
+                Logger.Exception(e.Exception);
 
             AppDomain.CurrentDomain.UnhandledException += (s, e) =>
             {
                 var ee = (Exception)e.ExceptionObject;
-                Logger.Exception(ee, ee.Message);
+                Logger.Exception(ee);
             };
 
         }
