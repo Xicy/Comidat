53e612e1c6caba0bee0e40d2e5d0d05170c06b0c
Umut Akkaya <umutakkaya1996@gmail.com>
2018-02-05 21:39:46 +0300

    added personal info in game object

--- Comidat.Viewer/Assets/Scripts/TagController.cs
+++ Comidat.Viewer/Assets/Scripts/TagController.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections;
-using UnityEditor;
 using UnityEngine;
 
 public class TagController : MonoBehaviour
 {
+    public GameObject TagsObject;
+
     // Update is called once per frame
     void Start()
     {
@@ -13,22 +14,28 @@ public class TagController : MonoBehaviour
 
     IEnumerator DoCheck()
     {
-        GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefab/Man.prefab");
+        GameObject prefab = Resources.Load<GameObject>("Man");
         for (; ; )
         {
             GameObject tag;
             MoveController mover;
             foreach (var tagWithLocation in DataContext.Instance.GetTagsLocation())
             {
-                tag = GameObject.Find("tag_" + tagWithLocation.TagId);
+                tag = TagsObject.Find("Tag_" + tagWithLocation.pos.TagId, true);
                 if (tag == null)
                 {
-                    tag = Instantiate(prefab);
-                    tag.name = "tag_" + tagWithLocation.TagId;
+                    tag = Instantiate(prefab, TagsObject.transform);
+                    var info = tag.GetComponent<TagInfo>();
+                    info.info = tagWithLocation.tag;
+                    info.LocInfo = tagWithLocation.pos;
+                    tag.GetComponentInChildren<TextMesh>().text = tagWithLocation.tag.TagFullName;
+                    tag.transform.position = new Vector3((float)tagWithLocation.pos.XPosition,
+                        (float)tagWithLocation.pos.YPosition, (float)tagWithLocation.pos.ZPosition);
+                    tag.name = "Tag_" + tagWithLocation.pos.TagId;
                 }
                 mover = tag.GetComponent<MoveController>();
-                mover.IsActive = !((DateTime.Now - tagWithLocation.RecordDateTime).TotalMinutes > 10);
-                mover.Move(new Vector3((float)tagWithLocation.XPosition, (float)tagWithLocation.YPosition, (float)tagWithLocation.ZPosition));
+                mover.IsActive = !((DateTime.Now - tagWithLocation.pos.RecordDateTime).TotalMinutes > 10);
+                mover.Move(new Vector3((float)tagWithLocation.pos.XPosition, (float)tagWithLocation.pos.YPosition, (float)tagWithLocation.pos.ZPosition));
             }
 
             yield return new WaitForSeconds(5);
