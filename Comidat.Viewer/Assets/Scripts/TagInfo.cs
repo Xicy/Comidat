dac5d650e9f8c8c6eed9145c972e6b75f88c932f
Umut Akkaya <umutakkaya1996@gmail.com>
2018-02-05 21:40:00 +0300

    personal info classes

---
+++ Comidat.Viewer/Assets/Scripts/TagInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Comidat.Data.Model;
+using UnityEngine;
+
+public class TagInfo : MonoBehaviour
+{
+    public TBLTag info;
+    public TBLPosition LocInfo;
+}
---
+++ Comidat.Viewer/Assets/Scripts/TagInfoViewer.cs
@@ -0,0 +1,29 @@
+﻿using Comidat.Data.Model;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TagInfoViewer : MonoBehaviour
+{
+    public Text Name;
+    public Text TC;
+    public Text Phone;
+    public Text Location;
+    public Text Description;
+    public Button Ok;
+    public bool isActive { set { gameObject.SetActive(value); } get { return gameObject.activeSelf; } }
+
+    void Start()
+    {
+        Ok.onClick.AddListener(delegate { isActive = false; });
+    }
+
+    public void UpdateInfo(TBLTag ti, Transform loc)
+    {
+        Name.text = ti.TagFirstName + " " + ti.TagLastName;
+        TC.text = ti.TagTCNo;
+        Phone.text = ti.TagMobilTelephone;
+        Location.text = loc.localPosition.ToString();
+        Description.text = ti.TagDescription;
+        isActive = true;
+    }
+}
