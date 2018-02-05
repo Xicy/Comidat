22470a1a386ecdb9b111670b39abe87d14abe7f0
Umut Akkaya <umutakkaya1996@gmail.com>
2018-02-05 21:36:52 +0300

    added for confuser

---
+++ Comidat.Viewer/Assets/Scripts/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+//[assembly: Obfuscation(Exclude = false, Feature = "strong name key:../../Comidat.snk")]
+[assembly: Obfuscation(Exclude = false, Feature = "+constants")]
+[assembly: Obfuscation(Exclude = false, Feature = "+anti ildasm")]
+[assembly: Obfuscation(Exclude = false, Feature = "+anti debug")]
+[assembly: Obfuscation(Exclude = false, Feature = "+resources(mode=dynamic)")]
+[assembly: Obfuscation(Exclude = false, Feature = "+ctrl flow(predicate=expression)")]
+[assembly: Obfuscation(Exclude = false, Feature = "+rename(mode=reversible,password=Umut1996)")]
