295788078cc6864a9ba58b49bef6592ed668f2c0
Umut Akkaya <umutakkaya1996@gmail.com>
2018-02-05 21:40:13 +0300

    testing route

---
+++ Comidat.Viewer/Assets/Scripts/PathController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathController : MonoBehaviour
+{
+
+    public Transform[] path;
+
+    public void AddPath(Transform desti)
+    {
+        
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Comidat.Viewer/Assets/Scripts/Route.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Route
+{
+    public Route(Vector3 pos)
+    {
+        position = pos;
+        prev = new List<int>();
+        next = new List<int>();
+    }
+    public Vector3 position;
+    public List<int> prev;
+    public List<int> next;
+}
