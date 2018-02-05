aaad92f0913a286cdcb7cc8a74b7ef97d93711b1
Umut Akkaya <umutakkaya1996@gmail.com>
2018-02-05 21:38:38 +0300

    show person info over screen

---
+++ Comidat.Viewer/Assets/Scripts/SelectOverMouse.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectOverMouse : MonoBehaviour
+{
+    public TagInfoViewer InfoPanel;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo))
+            {
+                TagInfo ti = hitInfo.transform.gameObject.GetComponent<TagInfo>();
+                InfoPanel.UpdateInfo(ti.info, hitInfo.transform);
+            }
+        }
+    }
+}
