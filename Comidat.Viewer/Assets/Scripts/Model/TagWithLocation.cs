36a2d53bbc12fba20d590bacfab2f98fe815f870
Umut Akkaya <umutakkaya1996@gmail.com>
2018-02-05 21:36:40 +0300

    updated for all informations

--- Comidat.Viewer/Assets/Scripts/Model/TagWithLocation.cs
+++ Comidat.Viewer/Assets/Scripts/Model/TagWithLocation.cs
@@ -4,13 +4,7 @@ namespace Comidat.Data.Model
 {
     public class TagWithLocation
     {
-        public string TagFirstName;
-        public string TagDescription;
-        public double XPosition;
-        public double YPosition;
-        public double ZPosition;
-        public long TagId;
-        public int MapId;
-        public DateTime RecordDateTime;
+        public TBLTag tag;
+        public TBLPosition pos;
     }
 }
