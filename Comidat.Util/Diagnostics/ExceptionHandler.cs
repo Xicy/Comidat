5a64286ffded089d0e022a005747d66690839d39
Umut Akkaya <umutakkaya1996@gmail.com>
2018-03-22 22:47:43 +0300

    EntryPointNotFoundException exception suspended

--- Comidat.Util/Diagnostics/ExceptionHandler.cs
+++ Comidat.Util/Diagnostics/ExceptionHandler.cs
@@ -15,11 +15,13 @@ namespace Comidat.Diagnostics
             };
 
             if (Debugger.IsAttached) return;
+
             AppDomain.CurrentDomain.FirstChanceException += (s, e) =>
                 Logger.Exception(e.Exception);
 
             AppDomain.CurrentDomain.UnhandledException += (s, e) =>
             {
+                if (e.ExceptionObject is EntryPointNotFoundException) return;
                 var ee = (Exception)e.ExceptionObject;
                 Logger.Exception(ee);
             };
